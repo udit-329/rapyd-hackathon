95fe8af15648d31e45f440328c41e189e2a030ac
KnlnKS <soulkks@hotmail.ca>
2021-06-13 01:31:24 -0400

    ✨ Make Widget Embeddable

--- widget/src/main.ts
+++ widget/src/main.ts
@@ -1,10 +1,8 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+export default new App({
+  target: document.querySelector("#rapyd"),
+  props: {
+    name: "world",
+  },
 });
-
-export default app;
