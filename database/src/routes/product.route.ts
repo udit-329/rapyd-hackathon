d98979917b51377066928b1f9fc658508cb10446
KnlnKS <soulkks@hotmail.ca>
2021-06-12 20:40:15 -0400

    🥅 Better Error Handling

--- database/src/routes/product.route.ts
+++ database/src/routes/product.route.ts
@@ -4,33 +4,31 @@ import { ProductModel } from "../models";
 const app = express();
 
 app.get("/product/:id", async (request, response) => {
-  const { id } = request.params;
-  const product = await ProductModel.findById(id);
-
   try {
-    response.send(product);
+    const { id } = request.params;
+    const product = await ProductModel.findById(id);
+    response.send({success: true, product});
   } catch (error) {
-    response.status(500).send(product);
+    response.status(500).send({success: false, ...error});
   }
 });
 
 app.get("/products", async (request, response) => {
-  const product = await ProductModel.find({});
-
   try {
-    response.send(product);
+    const product = await ProductModel.find({});
+    response.send({success: true, product});
   } catch (error) {
-    response.status(500).send(product);
+    response.status(500).send({success: false, ...error});
   }
 });
 
 app.post("/product", async (request, response) => {
-  const product = new ProductModel(request.body);
   try {
-    await product.save()
-    response.send(product);
+    const product = new ProductModel(request.body);
+    await product.save();
+    response.send({success: true, product});
   } catch (error) {
-    response.status(500).send(product);
+    response.status(500).send({success: false, ...error});
   }
 });
 
