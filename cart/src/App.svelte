27675e02e6f8132d535cdc66db557c410590d079
KnlnKS <soulkks@hotmail.ca>
2021-06-19 15:59:39 -0400

    🎉 Create Cart

---
+++ cart/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Popup from './components/Popup.svelte';
+
+  let ProductClicked = false;
+
+  function showProduct() {
+		ProductClicked = !ProductClicked
+    console.log("open")
+	}
+
+  function close() {
+    ProductClicked = !ProductClicked
+    console.log("close")
+  }
+</script>
+
+<div>
+    <button class="btn" on:click = {showProduct}>Buy</button>
+    {#if ProductClicked}
+      <Popup on:closePopup={close}></Popup>
+    {/if}
+</div>
+
+<style>
+  div {
+    text-align: center;
+    padding: 1em;
+    max-width: 250px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    div {
+      max-width: none;
+    }
+  }
+</style>
---
+++ cart/src/components/Popup.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function closePopup() {
+    dispatch("closePopup", {
+      text: "close",
+    });
+  }
+</script>
+
+<div class="box">
+  <button class="close-btn" on:click={closePopup}>X</button>
+</div>
+
+<style>
+  .box {
+    position: fixed;
+    height: 80vh;
+    width: 50vw;
+    background-color: aliceblue;
+    top: 10%;
+    margin: 0 auto;
+    padding: 15px;
+    left: 10%;
+    right: 10%;
+  }
+</style>
