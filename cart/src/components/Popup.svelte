ba114fea404c08fdfeb25de0e2072d241a48227f
KnlnKS <soulkks@hotmail.ca>
2021-06-19 21:59:57 -0400

    💄💫 Style and Animate Cart

--- cart/src/components/Popup.svelte
+++ cart/src/components/Popup.svelte
@@ -2,28 +2,70 @@
   import { createEventDispatcher } from "svelte";
 
   const dispatch = createEventDispatcher();
-
-  function closePopup() {
-    dispatch("closePopup", {
-      text: "close",
-    });
-  }
 </script>
 
-<div class="box">
-  <button class="close-btn" on:click={closePopup}>X</button>
+<div class="cart-wrapper">
+  <div class="cart">
+    <svg
+      class="cart-exit"
+      on:click={() =>
+        dispatch("closePopup", {
+          text: "close",
+        })}
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 0 24 24"
+      width="24px"
+      fill="#000000"
+      ><path d="M0 0h24v24H0V0z" fill="none" /><path
+        d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"
+      /></svg
+    >
+  </div>
 </div>
 
 <style>
-  .box {
+  .cart-wrapper {
     position: fixed;
-    height: 80vh;
-    width: 50vw;
-    background-color: aliceblue;
-    top: 10%;
-    margin: 0 auto;
-    padding: 15px;
-    left: 10%;
-    right: 10%;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    left: 0;
+  }
+  .cart {
+    z-index: 25;
+    position: fixed;
+    border-radius: 25px 0 0 25px;
+    width: 30vw;
+    padding: 1vw;
+    right: -40vw;
+    top: 0;
+    bottom: 0;
+    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+    -webkit-animation: slide 0.5s forwards;
+    -webkit-animation-delay: 0.1s;
+    animation: slide 0.5s forwards;
+    animation-delay: 0.1s;
+  }
+
+  @-webkit-keyframes slide {
+    100% {
+      right: 0;
+    }
+    0% {
+      right: -40vw;
+    }
+  }
+
+  @keyframes slide {
+    100% {
+      right: 0;
+    }
+    0% {
+      right: -40vw;
+    }
+  }
+  .cart-exit:hover {
+    cursor: pointer;
   }
 </style>
