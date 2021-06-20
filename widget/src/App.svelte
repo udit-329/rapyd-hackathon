95b1eb6173b1e54c45395b18507c3921d247a9c5
KnlnKS <soulkks@hotmail.ca>
2021-06-19 23:40:15 -0400

    💄 Style Widget

--- widget/src/App.svelte
+++ widget/src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { ProductResponse } from "./types";
-  import Popup from './components/Popup.svelte';
+  import Popup from "./components/Popup.svelte";
   export let _id: string;
 
   let ProductClicked = false;
@@ -9,53 +9,66 @@
     await (await fetch(`http://localhost:2001/product/${_id}`)).json())();
 
   function showProduct() {
-		ProductClicked = !ProductClicked
-    console.log("open")
-	}
+    ProductClicked = !ProductClicked;
+    console.log("open");
+  }
 
   function close() {
-    ProductClicked = !ProductClicked
-    console.log("close")
+    ProductClicked = !ProductClicked;
+    console.log("close");
   }
 </script>
 
-<div>
-  {#await fetchData}
-    <p>...waiting</p>
-  {:then data}
+{#await fetchData}
+  <p>...waiting</p>
+{:then data}
+  <div class="product-widget">
     <img src={data.product.images[0]} alt={data.product.name} />
-    <p>
-      Product id is: {_id}
-    </p>
+      <h3>
+        {data.product.name}
+      </h3>
+      <div class="product-widget-header" on:click={showProduct}>Buy</div>
+  </div>
 
-    <button class="btn" on:click = {showProduct}>Buy</button>
-    {#if ProductClicked}
-      <Popup productId={_id} imgSrc={data.product.images[0]} on:closePopup={close}></Popup>
-    {/if}
-
-  {:catch error}
-    <p>An error occurred!</p>
-  {/await}
-</div>
+  {#if ProductClicked}
+    <Popup
+      productId={_id}
+      imgSrc={data.product.images[0]}
+      on:closePopup={close}
+    />
+  {/if}
+{:catch error}
+  <p>An error occurred!</p>
+{/await}
 
 <style>
   div {
-    text-align: center;
-    padding: 1em;
-    max-width: 250px;
+    width: 250px;
     margin: 0 auto;
+  }
+
+  .product-widget {
+    border-radius: 25px;
+    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+    width: 250px;
+    text-align: center;
+    padding: 1em 0 0 0;
+  }
+
+  .product-widget-header{
+    border-radius: 0 0 25px 25px;
+    background-color: #2b49ee;
+    color: #fff;
+  }
+
+  .product-widget-header:hover {
+    background-color: hsl(231, 85%, 60%);
+    cursor: pointer;
   }
 
   img {
     max-width: 250px;
     max-height: 250px;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
